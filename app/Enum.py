01aa9c75aa23102193bf85c57f12db616809180e
IgorV <igor.nepocrytyh@mail.ru>
2026-02-12 14:18:26 +0800

    Перечисления вынесены в отдельный файл

---
+++ app/Enum.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class EducationLevel(Enum):
+    School = "School"
+    College = "College"
+    University = "University"
+
+class Lesson(Enum):
+    physics = "Physics"
+    mathematics = "Mathematics"
+    english = "English"
+    russian = "Russian"
+    geography = "Geography"
+    literature = "Literature"
