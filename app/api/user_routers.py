d2e8b4aa140159eb27f2f0d9bfdfa754659ba039
IgorV <igor.nepocrytyh@mail.ru>
2026-02-12 14:24:46 +0800

    Изменения взаимодействия роутеров и их crud моделей

--- app/api/user_routers.py
+++ app/api/user_routers.py
@@ -2,30 +2,22 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from .models import EducationLevel
-from ..database import SessionLocal
+from ..Schemas import UserCreate
+from ..database import get_db
 from ..crud.userCrud import get_user,create_user_crud
 from datetime import date
-
-router = APIRouter(prefix="/user",tags=['API'])
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from ..crud.userCrud import create_user_crud
 
 
-@router.post("/users/")
-def create_user(first_name: str, last_name: str,
-                middle_name:str,date_of_birth: date,
-                hash_password: str, education_level: EducationLevel,
-                db: Session = Depends(get_db)):
-    return create_user_crud(db=db, first_name=first_name, last_name=last_name,
-                       middle_name=middle_name, date_of_birth=date_of_birth,
-                       hash_password=hash_password, education_level=education_level)
+router = APIRouter(prefix="/user",tags=['Users'])
 
-@router.get("/users/{user_id}")
+
+@router.post("/create")
+def create_user_api(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user_crud(db, user)
+
+
+@router.get("/{user_id}")
 def read_user(user_id: int, db: Session = Depends(get_db)):
     user = get_user(db, user_id)
     if not user:
--- app/crud/userCrud.py
+++ app/crud/userCrud.py
@@ -2,21 +2,25 @@ from datetime import date
 
 from sqlalchemy import Date
 
+from ..Schemas import UserCreate
 from ..api.models import User, EducationLevel
 from ..security.Sec_pass import secu_me
 from sqlalchemy.orm import Session
 
 
-def create_user_crud(db: Session, first_name: str, last_name: str,
-                middle_name:str,date_of_birth: date,
-                hash_password: str, education_level: EducationLevel):
-    new_user = User(first_name=first_name, last_name=last_name,middle_name=middle_name,
-                    date_of_birth=date_of_birth,
-                    hash_password=secu_me(hash_password), education_level=education_level)
-    db.add(new_user)
+def create_user_crud(db: Session, user: UserCreate):
+    db_user = User(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        middle_name=user.middle_name,
+        date_of_birth=user.date_of_birth,
+        hash_password=secu_me(user.password),
+        education_level=user.education_level
+    )
+    db.add(db_user)
     db.commit()
-    db.refresh(new_user)
-    return {"msg": "Пользователь успешно зарегистрирован"}
+    db.refresh(db_user)
+    return db_user
 
 
 def delete_user(db: Session, user_id: int):
